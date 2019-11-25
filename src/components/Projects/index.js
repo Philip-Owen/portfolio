99cb33e505876d3eb2f834363f98664ac8e0739e
Philip-Owen <31570473+Philip-Owen@users.noreply.github.com>
2019-11-24 21:21:51 -0600

    refactored projects section to use hooks

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,42 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import RepoContainer from './RepoContainer';
 
-class Projects extends React.Component {
-	state = {
-		repos: [],
-		loaded: false,
+const Projects = () => {
+	const [repos, setRepos] = useState([]);
+	const [loaded, setLoaded] = useState(false);
+
+	const fetchRepos = async () => {
+			const res = await fetch('https://api.github.com/users/philip-owen/repos?sort=updated&per_page=5');
+			const data = await res.json();
+			setRepos(data);
+			setLoaded(!loaded);
 	};
 
-	componentDidMount() {
-		fetch('https://api.github.com/users/philip-owen/repos?sort=updated&per_page=5')
-			.then(data => data.json())
-			.then(json => {
-				this.setState({ repos: json, loaded: !this.state.loaded });
-			});
-	}
+	useEffect(() => {
+		fetchRepos();
+	}, []);
 
-	render() {
-		const { repos, loaded } = this.state;
-		return (
-			<Container id="projects">
-				<SectionTitle>
-					<h2>Projects</h2>
-				</SectionTitle>
-				<SectionSubtitle>What {"I've"} been working on:</SectionSubtitle>
-				<RepoContainer repos={repos} loaded={loaded} />
-				<p className="more-text">
-					Find more on my Github
-					<a href="https://www.github.com/philip-owen" target="_blank" rel="noopener noreferrer">
-						<FontAwesomeIcon icon={['fab', 'github']} color="#35495e" size="2x" />
-					</a>
-				</p>
-			</Container>
-		);
-	}
-}
+	return (
+		<Container id="projects">
+			<SectionTitle>
+				<h2>Projects</h2>
+			</SectionTitle>
+			<SectionSubtitle>What {"I've"} been working on:</SectionSubtitle>
+			<RepoContainer repos={repos} loaded={loaded} />
+			<p className="more-text">
+				Find more on my Github
+				<a href="https://www.github.com/philip-owen" target="_blank" rel="noopener noreferrer">
+					<FontAwesomeIcon icon={['fab', 'github']} color="#35495e" size="2x" />
+				</a>
+			</p>
+		</Container>
+	);
+};
 
 const Container = styled.div`
 	padding: 90px 50px;
@@ -44,7 +42,6 @@ const Container = styled.div`
 		text-align: center;
 	}
 `;
-
 const SectionTitle = styled.div`
 	font-size: 40px;
 	text-align: center;
